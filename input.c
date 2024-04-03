aa0607f8a1091cf53de41a1efacb171db80d8dbe
Chen1Plus <1plus.yijia@gmail.com>
2024-04-04 01:30:14 +0800

    float tested

---
+++ input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// haha i'm a handsome boy
+
+/* 66666666666666666 */
+
+/* // */
+
+/* */ //
+
+int main()
+{
+    float k = 89456.1231456;
+    float m = .4789456;
+    float n = 978456.;
+    int i = 123;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    /* printf("a + b = %d", a + b); */
+    return 0;
+}
